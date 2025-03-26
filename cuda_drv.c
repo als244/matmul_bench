450273a6922fbd1562810e24434af479b613045e
Andrew Sheinberg <andrew.sheinberg@gmail.com>
2025-03-25 20:41:42 -0400

    Testing again

--- cuda_drv.c
+++ cuda_drv.c
@@ -176,13 +176,18 @@ int initialize_ctx(int device_id, CUcontext * ctx, int num_sms, int * total_sms,
 			return -1;
 		}
 
-		*used_sms = result_sm_resources[0].sm.smCount;
-
-		printf("\n\nUsed SMs: %d\n", result_sm_resources[0].sm.smCount);
+		*used_sms = 0;
 
 		// Generate resource desc
 		CUdevResourceDesc sm_resource_desc;
 		unsigned int nbResources = MY_MAX(1, num_groups - 1);
+
+		for (int i = 0; i < nbResources; i++){
+			*used_sms += result_sm_resources[0].sm.smCount;
+		}
+
+		printf("\n\nUsed SMs: %d\n", result_sm_resources[0].sm.smCount);
+
 		result = cuDevResourceGenerateDesc(&sm_resource_desc, result_sm_resources, nbResources);
 		if (result != CUDA_SUCCESS){
 			cuGetErrorString(result, &err);
